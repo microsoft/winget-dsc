6638c1c681ed57c9aaf2c079fe929dba9919de99
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2022-10-06 17:37:31 -0700

    Correct unsigned modules

--- demo/c#/WinDSC/WinDSC.Core/PowerShell/Unsigned/WinDSC.psm1
+++ demo/c#/WinDSC/WinDSC.Core/PowerShell/Unsigned/WinDSC.psm1
@@ -1,154 +1,41 @@
-enum PackageState
-{
-    NotPresent
-    Installed
-}
+function Start-WinDSC {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $inputFile
+    )
 
-[DSCResource()]
-class WinDSCResourceDemo
-{
-    [DscProperty(Key)]
-    [string]$PackageIdentifier
+    Import-Module PSDesiredStateConfiguration
 
-    [DscProperty(Key)]
-    [string]$Version
-
-    [string]$Registry
-
-    [DscProperty()]
-    [PackageState]$PackageState
-
-    [WinDSCResourceDemo] Get()
-    {
-        Write-Verbose "Getting current state of $($this.PackageIdentifier) $($this.Version)"
-        $result = WinDSCResourceGetHelper -PackageIdentifier $this.PackageIdentifier -version $this.Version
-        return $result
+    # This is a hack
+    $parent = (Get-Item $PSScriptRoot).parent
+    if ($env:PSModulePath -notlike $parent) {
+        $env:PSModulePath += ";$parent"
     }
 
-    [bool] Test()
+    $obj = Get-Content $inputFile | ConvertFrom-Json
+
+    foreach ($package in $obj.packages)
     {
-        Write-Verbose "Testing $($this.PackageIdentifier) $($this.Version)"
-        $test = WinDSCResourceTestHelper -PackageIdentifier $this.PackageIdentifier -version $this.Version
-        return $test
-
-    }
-
-    [void] Set() {
-        Write-Verbose "Setting $($this.PackageIdentifier) $($this.Version)"
-        WinDSCResourceSetHelper -PackageIdentifier $this.PackageIdentifier -version $this.Version
-    }
-
-}
-
-function WinDSCResourceGetHelper
-{
-    [CmdletBinding()]
-    param([string]$packageIdentifier, [string]$version)
-
-    try
-    {
-        $state = IsRegistryKeyPresent -PackageIdentifier $packageIdentifier -version $version
-        $registry = GetRegistryKey -PackageIdentifier $packageIdentifier -version $version
-        $result = @{
-            PackageIdentifier = $PackageIdentifier
-            Version = $version
-            PackageState = $state
-            Registry = $registry
-        }
-    }
-    catch
-    {
-        throw $_
-    }
-
-    return $result
-}
-
-function WinDSCResourceTestHelper
-{
-    [CmdletBinding()]
-    param([string]$packageIdentifier, [string]$version)
-
-    try
-    {
-        $state = IsRegistryKeyPresent -PackageIdentifier $packageIdentifier -version $version
-        return $state -eq [PackageState]::Installed
-    }
-    catch
-    {
-        throw $_
-    }
-}
-
-function WinDSCResourceSetHelper
-{
-    [CmdletBinding()]
-    param([string]$packageIdentifier, [string]$version)
-
-    try
-    {
-        # We write to "HKLM:\SOFTWARE\SOFTWARE\WinDSCDemo\publisher\package\version for proff of concept.
-        $registryKey = "HKCU:\SOFTWARE\WinDSCDemo";
-
-        if (-Not (Test-Path $registryKey))
-        {
-            New-Item -Path "HKCU:\SOFTWARE" -Name "WinDSCDemo" -Force
-        }
-
-        $parts = $PackageIdentifier.Split('.');
-        foreach ($part in $parts)
-        {
-            $tmpRegistryKey = $registryKey + '\' + $part;
-            if (-Not (Test-Path $tmpRegistryKey))
-            {
-                New-Item -Path $registryKey -Name $part
+        Write-Host $package.packageIdentifier $package.version
+        $resource = @{
+            Name = 'WinDSCResourceDemo'
+            ModuleName = 'WinDSCResourceDemo'
+            Property = @{
+                PackageIdentifier = $package.packageIdentifier;
+                Version = $package.version;
             }
-            $registryKey = $tmpRegistryKey;
         }
+        
+        Write-Verbose -Verbose 'Set'
+        Invoke-DscResource @resource -Method Set
 
-        New-Item -Path $registryKey -Name $version -Force
-    }
-    catch
-    {
-        throw $_
-    }
-}
+        Write-Verbose -Verbose 'Test'
+        # Force is needed with Format-Table to show the result as PS tries to be too smart and sees a different type
+        # of object coming to the pipeline (after the set above) and thus doesn't render it
+        Invoke-DscResource @resource -Method Test | Format-Table -Force
 
-function GetRegistryKey
-{
-    [CmdletBinding()]
-    param([string]$packageIdentifier, [string]$version)
-
-    $registryKey = "HKCU:\SOFTWARE\WinDSCDemo";
-
-    $parts = $packageIdentifier.Split('.');
-    foreach ($part in $parts)
-    {
-        $registryKey += '\' + $part;
-    }
-
-    $registryKey += '\' + $version;
-    return $registryKey;
-}
-
-function IsRegistryKeyPresent
-{
-    [CmdletBinding()]
-    param([string]$packageIdentifier, [string]$version)
-
-    try
-    {
-        $registryKey = GetRegistryKey -PackageIdentifier $packageIdentifier -version $version
-
-        if (Test-Path $registryKey)
-        {
-            return [PackageState]::Installed
-        }
-
-        return [PackageState]::NotPresent
-    }
-    catch
-    {
-        throw $_
+        Write-Verbose -Verbose 'Get'
+        Invoke-DscResource @resource -Method Get | Format-Table -Force
     }
 }
