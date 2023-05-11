4a088dfb22bdced2b0de96562ee03f2e992238a5
Ryan <69221034+ryfu-msft@users.noreply.github.com>
2023-05-11 16:07:48 -0700

    Add initial Windows DSC Resource implementation (#8)
    This PR implements the Windows DSC Resource with the following resources:
    
    DeveloperMode:
    
    Used for enabling or disabling DeveloperMode using the Ensure Parameter
    OsVersion
    
    Used for Asserting that the current OSVersion satisfised the MinVersion Parameter
    Verified Manually.

---
+++ resources/Microsoft.Windows.DSC/Microsoft.Windows.DSC.psm1
@@ -0,0 +1,136 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+enum Ensure
+{
+    Absent
+    Present
+}
+
+#region DSCResources
+[DSCResource()]
+class DeveloperMode
+{
+    [DscProperty(Key)]
+    [Ensure] $Ensure = [Ensure]::Present
+
+    [DscProperty(NotConfigurable)]
+    [bool] $IsEnabled
+
+    [DeveloperMode] Get()
+    {
+        $this.IsEnabled = IsDeveloperModeEnabled
+
+        return @{
+            Ensure = $this.Ensure
+            IsEnabled = $this.IsEnabled
+        }       
+    }
+
+    [bool] Test()
+    {
+        $currentState = $this.Get()
+        if ($currentState.Ensure -eq [Ensure]::Present)
+        {
+            return $currentState.IsEnabled
+        }
+        else
+        {
+            return $currentState.IsEnabled -eq $false
+        }
+    }
+
+    [void] Set()
+    {
+        if (!$this.Test())
+        {
+            $windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+            $windowsPrincipal = New-Object -TypeName 'System.Security.Principal.WindowsPrincipal' -ArgumentList @( $windowsIdentity )
+        
+            if (-not $windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
+            {
+                throw "Toggling Developer Mode requires this resource to be run as an Administrator."
+            }
+
+            $shouldEnable = $this.Ensure -eq [Ensure]::Present
+            SetDeveloperMode -Enable $shouldEnable
+        }
+    }
+
+}
+
+[DSCResource()]
+class OsVersion
+{
+    [DscProperty(Key)]
+    [string] $MinVersion
+
+    [DscProperty(NotConfigurable)]
+    [string] $OsVersion
+
+    [OsVersion] Get()
+    {
+        $parsedVersion = $null
+        if (![System.Version]::TryParse($this.MinVersion, [ref]$parsedVersion))
+        {
+            throw "'$($this.MinVersion)' is not a valid Version string."
+        }
+
+        $this.OsVersion = (Get-ComputerInfo | Select-Object OsVersion).OsVersion
+
+        return @{
+            MinVersion = $this.MinVersion
+            OsVersion = $this.OsVersion
+        }
+    }
+
+    [bool] Test()
+    {
+        $currentState = $this.Get()
+        return [System.Version]$currentState.OsVersion -gt [System.Version]$currentState.MinVersion
+    }
+
+    [void] Set()
+    {
+        # This resource is only for asserting the os version requirement.
+    }
+
+}
+
+#endregion DSCResources
+
+#region Functions
+$AppModelUnlockRegistryKeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock\'
+$DeveloperModePropertyName = 'AllowDevelopmentWithoutDevLicense'
+
+function IsDeveloperModeEnabled
+{
+    try
+    {
+        $property = Get-ItemProperty -Path $AppModelUnlockRegistryKeyPath -Name $DeveloperModePropertyName
+        return $property.AllowDevelopmentWithoutDevLicense -eq 1
+    }
+    catch
+    {
+        # This will throw an exception if the registry path or property does not exist.
+        return $false;
+    }
+}
+
+function SetDeveloperMode
+{
+    param (
+        [Parameter(Mandatory)]
+        [bool]$Enable
+    )
+
+    if (-not (Test-Path -Path $AppModelUnlockRegistryKeyPath))
+    {
+        New-Item -Path $AppModelUnlockRegistryKeyPath -Force | Out-Null
+    }
+
+    $developerModeValue = [int]$Enable
+    New-ItemProperty -Path $AppModelUnlockRegistryKeyPath -Name $DeveloperModePropertyName -Value $developerModeValue -PropertyType DWORD -Force | Out-Null
+}
+
+#endregion Functions
