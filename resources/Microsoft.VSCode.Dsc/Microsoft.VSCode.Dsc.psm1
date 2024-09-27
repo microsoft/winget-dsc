17dec7198ad40618592cff055339115ba728efd8
Baby Grogu <Gijs.Reijn@rabobank.com>
2024-09-27 08:07:52 +0200

    Added new VSCodeInsiders class with Pester tests

--- resources/Microsoft.VSCode.Dsc/Microsoft.VSCode.Dsc.psm1
+++ resources/Microsoft.VSCode.Dsc/Microsoft.VSCode.Dsc.psm1
@@ -26,6 +26,27 @@ function Get-VSCodeCLIPath
     }
 }
 
+function Get-VSCodeInsidersCLIPath
+{
+    # Currently only supports user/machine install for VSCode on Windows.
+    # TODO: Update this function to handle when VSCode is installed in portable mode or on macOS/Linux.
+    $codeInsidersCLIUserPath = "$env:LocalAppData\Programs\Microsoft VS Code Insiders\bin\code-insiders.cmd"
+    $codeInsidersCLIMachinePath = "$env:ProgramFiles\Microsoft VS Code Insiders\bin\code-insiders.cmd"
+
+    if (Test-Path -Path $codeInsidersCLIUserPath)
+    {
+        return $codeInsidersCLIUserPath
+    }
+    elseif (Test-Path -Path $codeInsidersCLIMachinePath)
+    {
+        return $codeInsidersCLIMachinePath
+    }
+    else
+    {
+        throw "VSCode Insiders is not installed."
+    }
+}
+
 function Install-VSCodeExtension
 {
     [CmdletBinding()]
@@ -33,7 +54,11 @@ function Install-VSCodeExtension
         [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
         [string]$Name,
         [Parameter(ValueFromPipelineByPropertyName)]
-        [string]$Version
+        [string]$Version,
+
+        [Parameter(Mandatory = $false)]
+        [System.String]
+        $VSCodeCLIPath = (Get-VSCodeCLIPath)
     )
     
     begin
@@ -57,7 +82,7 @@ function Install-VSCodeExtension
     process
     {
         $installArgument = Get-VSCodeExtensionInstallArgument @PSBoundParameters
-        Invoke-VSCode -Command "--install-extension $installArgument"
+        Invoke-VSCode -Command "--install-extension $installArgument" -VSCodeCLIPath $VSCodeCLIPath
     }
 }
 
@@ -66,17 +91,25 @@ function Uninstall-VSCodeExtension
     [CmdletBinding()]
     param (
         [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
-        [string]$Name
+        [string]$Name,
+
+        [Parameter(Mandatory = $false)]
+        [System.String]
+        $VSCodeCLIPath = (Get-VSCodeCLIPath)
     )
         
-    Invoke-VSCode -Command "--uninstall-extension $($this.Name)"  
+    Invoke-VSCode -Command "--uninstall-extension $($this.Name)" -VSCodeCLIPath $VSCodeCLIPath 
 }
 
 function Invoke-VSCode
 {
     param (
         [Parameter(Mandatory = $true)]
-        [string]$Command
+        [string]$Command,
+
+        [Parameter(Mandatory = $false)]
+        [System.String]
+        $VSCodeCLIPath = (Get-VSCodeCLIPath)
     )
 
     try 
@@ -89,9 +122,6 @@ function Invoke-VSCode
     }
 }
 #endregion Functions
-
-# Keeps the path of the code.exe CLI path.
-$VSCodeCLIPath = Get-VSCodeCLIPath
 
 #region DSCResources
 [DSCResource()]
@@ -209,6 +239,130 @@ class VSCodeExtension
     {
         Uninstall-VSCodeExtension -Name $this.Name
         [VSCodeExtension]::GetInstalledExtensions()
+    }
+
+    [void] Uninstall()
+    {
+        $this.Uninstall($true)
+    }
+#endregion VSCodeExtension helper functions
+}
+
+[DSCResource()]
+class VSCodeInsidersExtension
+{
+    [DscProperty(Key)]
+    [string] $Name
+
+    [DscProperty()]
+    [string] $Version
+
+    [DscProperty()]
+    [bool] $Exist = $true
+
+    static [hashtable] $InstalledExtensions
+
+    static VSCodeInsidersExtension()
+    {
+        [VSCodeInsidersExtension]::GetInstalledExtensions()
+    }
+
+    static [VSCodeInsidersExtension[]] Export()
+    {
+        $extensionList = (Invoke-VSCode -Command "--list-extensions --show-versions" -VSCodeCLIPath (Get-VSCodeInsidersCLIPath)) -Split [Environment]::NewLine
+
+        $results = [VSCodeInsidersExtension[]]::new($extensionList.length)
+        
+        for ($i = 0; $i -lt $extensionList.length; $i++)
+        {
+            $extensionName, $extensionVersion = $extensionList[$i] -Split '@'
+            $results[$i] = [VSCodeInsidersExtension]@{
+                Name = $extensionName
+                Version = $extensionVersion
+            }     
+        }
+
+        return $results
+    }
+
+    [VSCodeInsidersExtension] Get()
+    {
+        $currentState = [VSCodeInsidersExtension]::InstalledExtensions[$this.Name]
+        if ($null -ne $currentState)
+        {
+            return $currentState
+        }
+        
+        return [VSCodeInsidersExtension]@{
+            Name = $this.Name
+            Version = $this.Version
+            Exist = $false
+        }
+    }
+
+    [bool] Test()
+    {
+        $currentState = $this.Get()
+        if ($currentState.Exist -ne $this.Exist)
+        {
+            return $false
+        }
+
+        if ($null -ne $this.Version -and $this.Version -ne $currentState.Version)
+        {
+            return $false
+        }
+
+        return $true
+    }
+
+    [void] Set()
+    {
+        if ($this.Test())
+        {
+            return
+        }
+
+        if ($this.Exist)
+        {
+            $this.Install($false)
+        }
+        else
+        {
+            $this.Uninstall($false)
+        }
+    }
+
+#region VSCodeInsidersExtension helper functions
+    static [void] GetInstalledExtensions()
+    {
+        [VSCodeInsidersExtension]::InstalledExtensions = @{}
+        foreach ($extension in [VSCodeInsidersExtension]::Export())
+        {
+            [VSCodeInsidersExtension]::InstalledExtensions[$extension.Name] = $extension
+        }          
+    }
+
+    [void] Install([bool] $preTest)
+    {
+        if ($preTest -and $this.Test())
+        {
+            return
+        }
+
+        Install-VSCodeExtension -Name $this.Name -Version $this.Version -VSCodeCLIPath (Get-VSCodeInsidersCLIPath)
+        [VSCodeInsidersExtension]::GetInstalledExtensions()
+    }
+
+    [void] Install()
+    {
+        $this.Install($true)
+    }
+
+    [void] Uninstall([bool] $preTest)
+    {
+        Uninstall-VSCodeExtension -Name $this.Name -VSCodeCLIPath (Get-VSCodeInsidersCLIPath)
+        [VSCodeInsidersExtension]::GetInstalledExtensions()
     }
 
     [void] Uninstall()
---
+++ tests/Microsoft.VSCode.Dsc/Microsoft.Windows.Dsc.Tests.ps1
@@ -0,0 +1,102 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+using module Microsoft.VSCode.Dsc
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+<#
+.Synopsis
+   Pester tests related to the Microsoft.VSCode.Dsc PowerShell module.
+#>
+
+BeforeAll {
+    Install-Module -Name PSDesiredStateConfiguration -Force -SkipPublisherCheck
+    Import-Module Microsoft.VSCode.Dsc
+
+    # Install VSCode 
+    Invoke-WebRequest https://raw.githubusercontent.com/PowerShell/vscode-powershell/main/scripts/Install-VSCode.ps1 -UseBasicParsing -OutFile Install-VSCode.ps1
+    .\Install-VSCode.ps1 -BuildEdition Stable-User -Confirm:$false
+
+    # Install VSCode Insiders
+    .\Install-VSCode.ps1 -BuildEdition Insider-User -Confirm:$false
+}
+
+Describe 'List available DSC resources' {
+    It 'Shows DSC Resources' {
+        $expectedDSCResources = "VSCodeExtension", "VSCodeInsidersExtension"
+        $availableDSCResources = (Get-DscResource -Module Microsoft.VSCode.Dsc).Name
+        $availableDSCResources.length | Should -Be 2
+        $availableDSCResources | Where-Object { $expectedDSCResources -notcontains $_ } | Should -BeNullOrEmpty -ErrorAction Stop
+    }
+}
+
+Describe 'VSCodeExtension' {
+    It 'Keeps current extension.' {
+        $parameters = @{
+            Name = 'ms-vscode.powershell'
+        }
+
+        $initialState = Invoke-DscResource -Name VSCodeExtension -ModuleName Microsoft.VSCode.Dsc -Method Get -Property $parameters
+ 
+        $testResult = Invoke-DscResource -Name VSCodeExtension -ModuleName Microsoft.VSCode.Dsc -Method Test -Property $parameters
+        $testResult.InDesiredState | Should -Be $true
+ 
+        # Invoking set should not change these values.
+        Invoke-DscResource -Name VSCodeExtension -ModuleName Microsoft.VSCode.Dsc -Method Set -Property $parameters
+        $finalState = Invoke-DscResource -Name VSCodeExtension -ModuleName Microsoft.VSCode.Dsc -Method Get -Property $parameters
+        $finalState.Name | Should -Be $initialState.Name
+        $finalState.Version | Should -Be $initialState.Version
+        $finalState.Exist | Should -Be $initialState.Exist
+    }
+
+    It 'Sets desired extension' {
+        $desiredState = @{
+            Name = 'ms-azure-devops.azure-pipelines'
+        }
+        
+        Invoke-DscResource -Name VSCodeExtension -ModuleName Microsoft.VSCode.Dsc -Method Set -Property $desiredState
+     
+        $finalState = Invoke-DscResource -Name Taskbar -ModuleName Microsoft.Windows.Developer -Method Get -Property $desiredState
+        $finalState.Name | Should -Be $desiredState.Name
+        # $finalState.Version | Should -Be $initialState.Version TODO: Add version check
+        $finalState.Exist | Should -BeTrue
+     }
+}
+
+Describe 'VSCodeInsidersExtension' {
+    It 'Keeps current extension.' {
+        $parameters = @{
+            Name = 'ms-vscode.powershell'
+        }
+
+        $initialState = Invoke-DscResource -Name VSCodeInsidersExtension -ModuleName Microsoft.VSCode.Dsc -Method Get -Property $parameters
+ 
+        $testResult = Invoke-DscResource -Name VSCodeInsidersExtension -ModuleName Microsoft.VSCode.Dsc -Method Test -Property $parameters
+        $testResult.InDesiredState | Should -Be $true
+ 
+        # Invoking set should not change these values.
+        Invoke-DscResource -Name VSCodeInsidersExtension -ModuleName Microsoft.VSCode.Dsc -Method Set -Property $parameters
+        $finalState = Invoke-DscResource -Name VSCodeInsidersExtension -ModuleName Microsoft.VSCode.Dsc -Method Get -Property $parameters
+        $finalState.Name | Should -Be $initialState.Name
+        $finalState.Version | Should -Be $initialState.Version
+        $finalState.Exist | Should -Be $initialState.Exist
+    }
+
+    It 'Sets desired extension' {
+        $desiredState = @{
+            Name = 'ms-azure-devops.azure-pipelines'
+        }
+        
+        Invoke-DscResource -Name VSCodeInsidersExtension -ModuleName Microsoft.VSCode.Dsc -Method Set -Property $desiredState
+     
+        $finalState = Invoke-DscResource -Name Taskbar -ModuleName Microsoft.Windows.Developer -Method Get -Property $desiredState
+        $finalState.Name | Should -Be $desiredState.Name
+        # $finalState.Version | Should -Be $initialState.Version TODO: Add version check
+        $finalState.Exist | Should -BeTrue
+     }
+}
+
+AfterAll {
+    # Uninstall VSCode?
+}
