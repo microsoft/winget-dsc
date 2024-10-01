7effb15dab674e7756dc3411be522da8ac8af5c0
Baby Grogu <Gijs.Reijn@rabobank.com>
2024-10-01 09:51:18 +0200

    Rewritten VSCodeCLIPath

--- resources/Microsoft.VSCode.Dsc/Microsoft.VSCode.Dsc.psm1
+++ resources/Microsoft.VSCode.Dsc/Microsoft.VSCode.Dsc.psm1
@@ -5,50 +5,47 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 #region Functions
-function Get-VSCodeCLIPath
-{
+function Get-VSCodeCLIPath {
+    param (
+        [switch]$UseInsiders,
+        [string]$CustomPath
+    )
+
     # Currently only supports user/machine install for VSCode on Windows.
     # TODO: Update this function to handle when VSCode is installed in portable mode or on macOS/Linux.
-    $codeCLIUserPath = "$env:LocalAppData\Programs\Microsoft VS Code\bin\code.cmd"
-    $codeCLIMachinePath = "$env:ProgramFiles\Microsoft VS Code\bin\code.cmd"
 
-    if (Test-Path -Path $codeCLIUserPath)
-    {
+    # If a custom path is provided, use it
+    # if ($PSBoundParameters.ContainsKey('CustomPath')) { 
+    #     if ((Test-Path -Path $CustomPath) -and (Split-Path $CustomPath -Leaf) -in ('code.cmd', 'code-insiders.cmd')) {
+    #         return $CustomPath
+    #     } else {
+    #         throw "The specified custom path does not exist or does not start with 'code.cmd' or 'code-insiders.cmd'."
+    #     }
+    # }
+
+    # Determine the paths based on whether the Insiders version is used
+    if ($UseInsiders) {
+        $codeCLIUserPath = "$env:LocalAppData\Programs\Microsoft VS Code Insiders\bin\code-insiders.cmd"
+        $codeCLIMachinePath = "$env:ProgramFiles\Microsoft VS Code Insiders\bin\code-insiders.cmd"
+    }
+    else {
+        $codeCLIUserPath = "$env:LocalAppData\Programs\Microsoft VS Code\bin\code.cmd"
+        $codeCLIMachinePath = "$env:ProgramFiles\Microsoft VS Code\bin\code.cmd"
+    }
+
+    # Check the paths and return the appropriate one
+    if (Test-Path -Path $codeCLIUserPath) {
         return $codeCLIUserPath
     }
-    elseif (Test-Path -Path $codeCLIMachinePath)
-    {
+    elseif (Test-Path -Path $codeCLIMachinePath) {
         return $codeCLIMachinePath
     }
-    else
-    {
+    else {
         throw "VSCode is not installed."
     }
 }
 
-function Get-VSCodeInsidersCLIPath
-{
-    # Currently only supports user/machine install for VSCode on Windows.
-    # TODO: Update this function to handle when VSCode is installed in portable mode or on macOS/Linux.
-    $codeInsidersCLIUserPath = "$env:LocalAppData\Programs\Microsoft VS Code Insiders\bin\code-insiders.cmd"
-    $codeInsidersCLIMachinePath = "$env:ProgramFiles\Microsoft VS Code Insiders\bin\code-insiders.cmd"
-
-    if (Test-Path -Path $codeInsidersCLIUserPath)
-    {
-        return $codeInsidersCLIUserPath
-    }
-    elseif (Test-Path -Path $codeInsidersCLIMachinePath)
-    {
-        return $codeInsidersCLIMachinePath
-    }
-    else
-    {
-        throw "VSCode Insiders is not installed."
-    }
-}
-
-function Install-VSCodeExtension
-{
+function Install-VSCodeExtension {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
@@ -61,14 +58,11 @@ function Install-VSCodeExtension
         $VSCodeCLIPath = (Get-VSCodeCLIPath)
     )
     
-    begin
-    {
-        function Get-VSCodeExtensionInstallArgument
-        {
+    begin {
+        function Get-VSCodeExtensionInstallArgument {
             param([string]$Name, [string]$Version)
             
-            if ([string]::IsNullOrEmpty($Version))
-            {
+            if ([string]::IsNullOrEmpty($Version)) {
                 return $Name
             }
 
@@ -79,15 +73,13 @@ function Install-VSCodeExtension
         }
     }
     
-    process
-    {
+    process {
         $installArgument = Get-VSCodeExtensionInstallArgument @PSBoundParameters
         Invoke-VSCode -Command "--install-extension $installArgument" -VSCodeCLIPath $VSCodeCLIPath
     }
 }
 
-function Uninstall-VSCodeExtension
-{
+function Uninstall-VSCodeExtension {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
@@ -101,32 +93,24 @@ function Uninstall-VSCodeExtension
     Invoke-VSCode -Command "--uninstall-extension $($this.Name)" -VSCodeCLIPath $VSCodeCLIPath 
 }
 
-function Invoke-VSCode
-{
+function Invoke-VSCode {
     param (
         [Parameter(Mandatory = $true)]
-        [string]$Command,
-
-        [Parameter(Mandatory = $false)]
-        [System.String]
-        $VSCodeCLIPath = (Get-VSCodeCLIPath)
+        [string]$Command
     )
 
-    try 
-    {
+    try {
         Invoke-Expression "& `"$VSCodeCLIPath`" $Command"
     }
-    catch
-    {
-        throw "Executing code.exe with {$Command} failed."
+    catch {
+        throw ("Executing {0} with {$Command} failed." -f $VSCodeCLIPath)
     }
 }
 #endregion Functions
 
 #region DSCResources
 [DSCResource()]
-class VSCodeExtension
-{
+class VSCodeExtension {
     [DscProperty(Key)]
     [string] $Name
 
@@ -136,24 +120,36 @@ class VSCodeExtension
     [DscProperty()]
     [bool] $Exist = $true
 
-    static [hashtable] $InstalledExtensions
+    [System.Management.Automation.HiddenAttribute()]
+    [DscProperty()]
+    [System.Boolean]
+    $UseInsiders = $false
 
-    static VSCodeExtension()
-    {
-        [VSCodeExtension]::GetInstalledExtensions()
+    static [hashtable] $InstalledExtensions
+    
+    # VSCodeExtension() {
+    #     $this.Initialize($this.UseInsiders)
+    #     [VSCodeExtension]::InstalledExtensions = @{}
+    # }
+
+    static [void] Initialize([bool]$UseInsiders) {
+        [VSCodeExtension]::GetInstalledExtensions($UseInsiders) 
     }
 
-    static [VSCodeExtension[]] Export()
-    {
+    VSCodeExtension() {
+        # [VSCodeExtension]::Initialize($this.UseInsiders)
+        [VSCodeExtension]::InstalledExtensions = @{}
+    }
+
+    static [VSCodeExtension[]] Export([bool]$UseInsiders) {
         $extensionList = (Invoke-VSCode -Command "--list-extensions --show-versions") -Split [Environment]::NewLine
 
         $results = [VSCodeExtension[]]::new($extensionList.length)
         
-        for ($i = 0; $i -lt $extensionList.length; $i++)
-        {
+        for ($i = 0; $i -lt $extensionList.length; $i++) {
             $extensionName, $extensionVersion = $extensionList[$i] -Split '@'
             $results[$i] = [VSCodeExtension]@{
-                Name = $extensionName
+                Name    = $extensionName
                 Version = $extensionVersion
             }     
         }
@@ -161,68 +157,58 @@ class VSCodeExtension
         return $results
     }
 
-    [VSCodeExtension] Get()
-    {
+    [VSCodeExtension] Get() {
         $currentState = [VSCodeExtension]::InstalledExtensions[$this.Name]
-        if ($null -ne $currentState)
-        {
-            return $currentState
+        if ($null -ne $currentState) {
+            return [VSCodeExtension]::InstalledExtensions[$this.Name]
         }
         
         return [VSCodeExtension]@{
-            Name = $this.Name
+            Name    = $this.Name
             Version = $this.Version
-            Exist = $false
+            Exist   = $false
         }
     }
 
-    [bool] Test()
-    {
+    [bool] Test() {
         $currentState = $this.Get()
-        if ($currentState.Exist -ne $this.Exist)
-        {
+        if ($currentState.Exist -ne $this.Exist) {
             return $false
         }
 
-        if ($null -ne $this.Version -and $this.Version -ne $currentState.Version)
-        {
+        if ($null -ne $this.Version -and $this.Version -ne $currentState.Version) {
             return $false
         }
 
         return $true
     }
 
-    [void] Set()
-    {
-        if ($this.Test())
-        {
+    [void] Set() {
+        if ($this.Test()) {
             return
         }
 
-        if ($this.Exist)
-        {
+        if ($this.Exist) {
             $this.Install($false)
         }
-        else
-        {
+        else {
             $this.Uninstall($false)
         }
     }
 
-#region VSCodeExtension helper functions
-    static [void] GetInstalledExtensions()
-    {
+    #region VSCodeExtension helper functions
+    static [void] GetInstalledExtensions([bool]$UseInsiders) {   
         [VSCodeExtension]::InstalledExtensions = @{}
-        foreach ($extension in [VSCodeExtension]::Export())
-        {
+
+        $script:VSCodeCLIPath = Get-VSCodeCLIPath -UseInsiders:$UseInsiders
+
+        foreach ($extension in [VSCodeExtension]::Export($UseInsiders)) {
             [VSCodeExtension]::InstalledExtensions[$extension.Name] = $extension
-        }          
+        }     
     }
 
-    [void] Install([bool] $preTest)
-    {
-        if ($preTest -and $this.Test())
-        {
+    [void] Install([bool] $preTest) {
+        if ($preTest -and $this.Test()) {
             return
         }
 
@@ -230,145 +216,18 @@ class VSCodeExtension
         [VSCodeExtension]::GetInstalledExtensions()
     }
 
-    [void] Install()
-    {
+    [void] Install() {
         $this.Install($true)
     }
 
-    [void] Uninstall([bool] $preTest)
-    {
+    [void] Uninstall([bool] $preTest) {
         Uninstall-VSCodeExtension -Name $this.Name
         [VSCodeExtension]::GetInstalledExtensions()
     }
 
-    [void] Uninstall()
-    {
+    [void] Uninstall() {
         $this.Uninstall($true)
     }
-#endregion VSCodeExtension helper functions
-}
-
-[DSCResource()]
-class VSCodeInsidersExtension
-{
-    [DscProperty(Key)]
-    [string] $Name
-
-    [DscProperty()]
-    [string] $Version
-
-    [DscProperty()]
-    [bool] $Exist = $true
-
-    static [hashtable] $InstalledExtensions
-
-    static VSCodeInsidersExtension()
-    {
-        [VSCodeInsidersExtension]::GetInstalledExtensions()
-    }
-
-    static [VSCodeInsidersExtension[]] Export()
-    {
-        $extensionList = (Invoke-VSCode -Command "--list-extensions --show-versions" -VSCodeCLIPath (Get-VSCodeInsidersCLIPath)) -Split [Environment]::NewLine
-
-        $results = [VSCodeInsidersExtension[]]::new($extensionList.length)
-        
-        for ($i = 0; $i -lt $extensionList.length; $i++)
-        {
-            $extensionName, $extensionVersion = $extensionList[$i] -Split '@'
-            $results[$i] = [VSCodeInsidersExtension]@{
-                Name = $extensionName
-                Version = $extensionVersion
-            }     
-        }
-
-        return $results
-    }
-
-    [VSCodeInsidersExtension] Get()
-    {
-        $currentState = [VSCodeInsidersExtension]::InstalledExtensions[$this.Name]
-        if ($null -ne $currentState)
-        {
-            return $currentState
-        }
-        
-        return [VSCodeInsidersExtension]@{
-            Name = $this.Name
-            Version = $this.Version
-            Exist = $false
-        }
-    }
-
-    [bool] Test()
-    {
-        $currentState = $this.Get()
-        if ($currentState.Exist -ne $this.Exist)
-        {
-            return $false
-        }
-
-        if ($null -ne $this.Version -and $this.Version -ne $currentState.Version)
-        {
-            return $false
-        }
-
-        return $true
-    }
-
-    [void] Set()
-    {
-        if ($this.Test())
-        {
-            return
-        }
-
-        if ($this.Exist)
-        {
-            $this.Install($false)
-        }
-        else
-        {
-            $this.Uninstall($false)
-        }
-    }
-
-#region VSCodeInsidersExtension helper functions
-    static [void] GetInstalledExtensions()
-    {
-        [VSCodeInsidersExtension]::InstalledExtensions = @{}
-        foreach ($extension in [VSCodeInsidersExtension]::Export())
-        {
-            [VSCodeInsidersExtension]::InstalledExtensions[$extension.Name] = $extension
-        }          
-    }
-
-    [void] Install([bool] $preTest)
-    {
-        if ($preTest -and $this.Test())
-        {
-            return
-        }
-
-        Install-VSCodeExtension -Name $this.Name -Version $this.Version -VSCodeCLIPath (Get-VSCodeInsidersCLIPath)
-        [VSCodeInsidersExtension]::GetInstalledExtensions()
-    }
-
-    [void] Install()
-    {
-        $this.Install($true)
-    }
-
-    [void] Uninstall([bool] $preTest)
-    {
-        Uninstall-VSCodeExtension -Name $this.Name -VSCodeCLIPath (Get-VSCodeInsidersCLIPath)
-        [VSCodeInsidersExtension]::GetInstalledExtensions()
-    }
-
-    [void] Uninstall()
-    {
-        $this.Uninstall($true)
-    }
-#endregion VSCodeExtension helper functions
+    #endregion VSCodeExtension helper functions
 }
 #endregion DSCResources
