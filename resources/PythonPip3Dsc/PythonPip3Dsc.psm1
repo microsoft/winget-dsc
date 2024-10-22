d5aa84e1339bee3c978442f2caeba1ffdde2278f
Gijs Reijn <gijsreijn@hotmail.com>
2024-10-22 10:05:29 +0200

    Include export and version property

--- resources/PythonPip3Dsc/PythonPip3Dsc.psm1
+++ resources/PythonPip3Dsc/PythonPip3Dsc.psm1
@@ -3,113 +3,38 @@
 
 using namespace System.Collections.Generic
 
-$global:usePip3Exe = $false
-$global:pip3ExePath = "$env:LOCALAPPDATA\Programs\Python\Python38\Scripts\pip3.exe"
-
-# Assert once that pip3 is already installed on the system.
-Assert-Pip3
-
-enum Ensure
-{
-    Absent
-    Present
-}
-
-#region DSCResources
-[DSCResource()]
-class Pip3Package
-{
-    # DSCResource requires a key. Do not set.
-    [DscProperty(Key)]
-    [string]$SID
-
-    [DscProperty()]
-    [Ensure] $Ensure = [Ensure]::Present
-
-    [DscProperty(Mandatory)]
-    [string[]]$Packages
-
-    [DscProperty()]
-    [string]$Arguments
-
-    [DscProperty(NotConfigurable)]
-    [bool]$InstalledStatus
-
-    [DscProperty(NotConfigurable)]
-    [string[]]$InstalledPackages
-
-    [Pip3Package] Get()
-    {
-        $this.InstalledPackages = GetInstalledPip3Packages | ConvertFrom-Json | Select-Object -ExpandProperty name
-        $this.InstalledStatus = $true
-        foreach ($package in $this.Packages)
-        {
-            $this.InstalledStatus = $this.InstalledPackages -contains $package
-
-            if ($this.InstalledStatus -eq $false)
-            {
-                break
-            }
-        }
-
-        return @{
-            SID = $this.SID
-            Ensure = $this.Ensure
-            Packages = $this.Packages
-            Arguments = $this.Arguments
-            InstalledStatus = $this.InstalledStatus
-            InstalledPackages = $this.InstalledPackages
-        }
-    }
-
-    [bool] Test()
-    {
-        $currentState = $this.Get()
-        if ($currentState.Ensure -eq [Ensure]::Present)
-        {
-            return $currentState.InstalledStatus -eq $true
-        }
-        else
-        {
-            return $currentState.InstalledStatus -eq $false
-        }
-    }
-
-    [void] Set()
-    {
-        $currentState = $this.Get()
-        if ($currentState.Ensure -eq [Ensure]::Present)
-        {
-            if ($currentState.InstalledStatus -ne $true)
-            {
-                foreach ($package in $this.Packages)
-                {
-                    if ($currentState.InstalledPackages -notcontains $package)
-                    {
-                        Invoke-Pip3Install -PackageName $package -Arguments $this.Arguments
-                    }
-                }
-            }
-        }
-        else
-        {
-            if ($currentState.InstalledStatus -ne $false)
-            {
-                foreach ($package in $this.Packages)
-                {
-                    if ($currentState.InstalledPackages -contains $package)
-                    {
-                        Invoke-Pip3Uninstall -PackageName $package -Arguments $this.Arguments
-                    }
-                }
-            }
-        }
-    }
-}
-
-#endregion DSCResources
-
 #region Functions
+function Get-Pip3Path 
+{
+    if ($IsWindows)
+    {
+        # Note: When installing 64-bit version, the registry key: HKLM:\SOFTWARE\Wow6432Node\Python\PythonCore\*\InstallPath was empty.
+        $userUninstallRegistry = "HKCU:\SOFTWARE\Python\PythonCore\*\InstallPath"
+        $machineUninstallRegistry = "HKLM:\SOFTWARE\Python\PythonCore\*\InstallPath"
+        $installLocationProperty = "ExecutablePath"
+
+        $pythonExe = TryGetRegistryValue -Key $userUninstallRegistry -Property $installLocationProperty
+        $userInstallLocation = Join-Path (Split-Path $pythonExe -Parent) "Scripts\pip3.exe"
+        if ($userInstallLocation)
+        {
+            return $userInstallLocation
+        }
+
+        $pythonExe = TryGetRegistryValue -Key $machineUninstallRegistry -Property $installLocationProperty
+        $machineInstallLocation = Join-Path (Split-Path $pythonExe -Parent) "Scripts\pip3.exe"
+        if ($machineInstallLocation)
+        {
+            return $machineInstallLocation
+        }
+    }
+    elseif ($IsMacOS)
+    {
+        # TODO: add support for MacOS
+    }
+    
+    throw "Pip3 is not installed."
+}
+
 function Assert-Pip3
 {
     # Try invoking pip3 help with the alias. If it fails, switch to calling npm.cmd directly.
@@ -124,12 +49,53 @@ function Assert-Pip3
     if (Test-Path -Path $global:pip3ExePath)
     {
         $global:usePip3Exe = $true;
-        Write-Host "Calling pip3.exe from default install location: $global:usePip3Exe"
+        Write-Verbose "Calling pip3.exe from default install location: $global:usePip3Exe"
     }
     else
     {
         throw "Python is not installed"
     }
+}
+
+function Get-PackageNameWithVersion {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$PackageName,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Arguments,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Version
+    )
+
+    if ($PSBoundParameters.ContainsKey('Version') -and -not ([string]::IsNullOrEmpty($Version)))
+    {
+        $packageName = $PackageName + "==" + $Version
+    }
+
+    return $packageName
+}
+
+function Invoke-Pip3Update
+{
+    param (
+        [Parameter()]
+        [string]$PackageName,
+
+        [Parameter()]
+        [string]$Arguments,
+
+        [Parameter()]
+        [string]$Version
+    )
+
+    $command = [List[string]]::new()
+    $command.Add("install")
+    $command.Add((Get-PackageNameWithVersion @PSBoundParameters))
+    $command.Add("--force-reinstall")
+    $command.Add($Arguments)
+    return Invoke-Pip3 -command $command
 }
 
 function Invoke-Pip3Install
@@ -139,12 +105,15 @@ function Invoke-Pip3Install
         [string]$PackageName,
 
         [Parameter()]
-        [string]$Arguments
+        [string]$Arguments,
+
+        [Parameter()]
+        [string]$Version
     )
 
     $command = [List[string]]::new()
     $command.Add("install")
-    $command.Add($PackageName)
+    $command.Add((Get-PackageNameWithVersion @PSBoundParameters))
     $command.Add($Arguments)
     return Invoke-Pip3 -command $command
 }
@@ -156,17 +125,62 @@ function Invoke-Pip3Uninstall
         [string]$PackageName,
 
         [Parameter()]
-        [string]$Arguments
+        [string]$Arguments,
+
+        [Parameter()]
+        [string]$Version
     )
 
     $command = [List[string]]::new()
     $command.Add("uninstall")
-    $command.Add($PackageName)
+    $command.Add((Get-PackageNameWithVersion @PSBoundParameters))
     $command.Add($Arguments)
 
     # '--yes' is needed to ignore confrimation required for uninstalls
     $command.Add("--yes")
     return Invoke-Pip3 -command $command
+}
+
+function GetPip3CurrentState {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [AllowNull()]
+        [hashtable[]] $Package,
+
+        [Parameter(Mandatory = $true)]
+        [AllowNull()] 
+        [hashtable] $Parameters
+    )
+
+    if ($Parameters.ContainsKey('InstalledPackages'))
+    {
+        $Parameters.Remove('InstalledPackages')
+    }
+
+    $out = @{
+        Exist = $false
+        Package = $Parameters.Package
+        Version = $Parameters.Version
+        Arguments = $Parameters.Arguments
+        InstalledPackages = $Package
+    }
+
+    foreach ($entry in $Package)
+    {
+        if ($entry.name -eq $Parameters.Package)
+        {
+            $out.Exist = $true
+            $out.Version = $entry.version
+
+            if ($Parameters.ContainsKey('version') -and $entry.version -ne $Parameters.version)
+            {
+                $out.Exist = $false
+            }
+        }
+    }
+
+    return $out
 }
 
 function GetInstalledPip3Packages
@@ -175,7 +189,6 @@ function GetInstalledPip3Packages
     $Arguments.Add("list")
     $Arguments.Add("--format=json")
 
-    $command;
     if ($global:usePip3Exe)
     {
         $command = "& '$global:pip3ExePath' " + $Arguments
@@ -205,4 +218,181 @@ function Invoke-Pip3
     }
 }
 
+function TryGetRegistryValue{
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Key,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Property
+        )    
+
+    if (Test-Path -Path $Key)
+    {
+        try
+        {
+            return (Get-ItemProperty -Path $Key | Select-Object -ExpandProperty $Property)     
+        }
+        catch
+        {
+            Write-Verbose "Property `"$($Property)`" could not be found."
+        }
+    }
+    else
+    {
+        Write-Verbose "Registry key does not exist."
+    }
+}
+
 #endregion Functions
+
+$global:usePip3Exe = $false
+$global:pip3ExePath = Get-Pip3Path
+
+# Assert once that pip3 is already installed on the system.
+Assert-Pip3
+
+#region DSCResources
+<#
+.SYNOPSIS
+    The `Pip3Package` DSC Resource allows you to install, update, and uninstall Python packages using pip3.
+
+.PARAMETER SID
+    The security identifier. This is a key property and should not be set manually.
+
+.PARAMETER Exist
+    Indicates whether the package should exist. Defaults to $true.
+
+.PARAMETER Package
+    The name of the Python package to manage. This is a mandatory property.
+
+    For a list of Python packages, see https://pypi.org/.
+
+.PARAMETER Version
+    The version of the Python package to manage. If not specified, the latest version will be used.
+
+.PARAMETER Arguments
+    Additional arguments to pass to pip3.
+
+.PARAMETER InstalledPackages
+    A list of installed packages. This property is not configurable.
+
+.EXAMPLE
+    PS C:\> Invoke-DscResource -ModuleName PythonPip3Dsc -Name Pip3Package -Method Set -Property @{ Package = 'flask' }
+
+    This example installs the Flask package.
+
+.EXAMPLE
+    PS C:\> Invoke-DscResource -ModuleName PythonPip3Dsc -Name Pip3Package -Method Get -Property @{ Package = 'flask'; Version = '1.1.4' }
+
+    This example shows how to get the current state of the Flask package with version. If the version is not found, the latest version will be used if flask is found.
+
+.EXAMPLE
+    PS C:\> Invoke-DscResource -ModuleName PythonPip3Dsc -Name Pip3Package -Method Get -Property @{ Package = 'django'; Exist = $false }
+
+    This example shows how Django can be removed from the system.
+#>
+[DSCResource()]
+class Pip3Package
+{
+    # DSCResource requires a key. Do not set.
+    [DscProperty(Key)]
+    [string]$SID
+
+    [DscProperty()]
+    [bool] $Exist = $true
+
+    [DscProperty(Mandatory)]
+    [string]$Package
+
+    [DscProperty()]
+    [string]$Version
+
+    [DscProperty()]
+    [string]$Arguments
+
+    [DscProperty(NotConfigurable)]
+    [hashtable[]]$InstalledPackages
+
+    [Pip3Package] Get()
+    {
+        $this.InstalledPackages = (GetInstalledPip3Packages | ConvertFrom-Json -AsHashtable)
+        $currentState = GetPip3CurrentState -Package $this.InstalledPackages -Parameters $this.ToHashTable()
+
+        return $currentState
+    }
+
+    [bool] Test()
+    {
+        $currentState = $this.Get()
+        if ($currentState.Exist -eq $this.Exist -and $currentState.version -eq $this.Version)
+        {
+            return $true
+        }
+        else
+        {
+            return $false
+        }
+    }
+
+    [void] Set()
+    {
+        if ($this.Test())
+        {
+            return 
+        }
+
+        $currentPackage = $this.InstalledPackages[$this.Package]
+        if ($currentPackage -and $this.Exist -and $this.Version -ne $currentPackage.version)
+        {
+            Invoke-Pip3Update -PackageName $this.Package -Arguments $this.Arguments
+        }
+        elseif ($this.Exist)
+        {
+            Invoke-Pip3Install -PackageName $this.Package -Version $this.Version -Arguments $this.Arguments
+        }
+        else
+        {
+            Invoke-Pip3Uninstall -PackageName $this.Package -Version $this.Version -Arguments $this.Arguments
+        }
+    }
+
+    static [Pip3Package[]] Export()
+    {
+        $packages = GetInstalledPip3Packages | ConvertFrom-Json -AsHashtable
+        $out = [List[Pip3Package]]::new()
+        foreach ($package in $packages)
+        {
+            $in = [Pip3Package]@{
+                SID = $null
+                Package = $package.name
+                Version = $package.version
+                Exist = $true 
+                Arguments = $null
+                InstalledPackages = $packages
+            }
+
+            $out.Add($in)
+        }
+
+        return $out
+    }
+
+    #region Pip3Package Helper functions
+    [hashtable] ToHashTable()
+    {
+        $parameters = @{}
+        foreach ($property in $this.PSObject.Properties)
+        {
+            if (-not ([string]::IsNullOrEmpty($property.Value)))
+            {
+                $parameters[$property.Name] = $property.Value
+            }
+        }
+
+        return $parameters
+    }
+    #endRegion Pip3Package Helper functions
+}
+
+#endregion DSCResources
