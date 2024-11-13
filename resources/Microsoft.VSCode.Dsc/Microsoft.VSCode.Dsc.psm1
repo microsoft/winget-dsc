04f6db267febbfd289ee7bf087a64ca4a3d799ab
Gijs Reijn <gijsreijn@hotmail.com>
2024-11-13 21:47:35 +0100

    Remarks Ryan

--- resources/Microsoft.VSCode.Dsc/Microsoft.VSCode.Dsc.psm1
+++ resources/Microsoft.VSCode.Dsc/Microsoft.VSCode.Dsc.psm1
@@ -95,8 +95,8 @@ function Invoke-VSCode {
         [string]$Command
     )
 
-    $stdErrTempFile = [System.IO.Path]::GetTempPath() + (New-Guid).Guid
-    $stdOutTempFile = [System.IO.Path]::GetTempPath() + (New-Guid).Guid
+    $stdErrTempFile = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath (New-Guid).Guid
+    $stdOutTempFile = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath (New-Guid).Guid
     $invocationSuccess = $true
 
     $processParams = @{
