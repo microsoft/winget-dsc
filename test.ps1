ddf4f5af808e863a4dc1a60cbfa5fef524023a33
G.Reijn <G.Reijn@enzazaden.nl>
2025-01-10 16:18:49 +0100

    Fix spelling

---
+++ test.ps1
@@ -0,0 +1,3 @@
+﻿$class = [Windows.Services.Maps.OfflineMaps.OfflineMapPackage, Windows.Services.Maps.OfflineMaps, ContentType = WindowsRuntime]
+
+# https://github.com/PowerShell/PowerShell/issues/13042#issuecomment-653357546
