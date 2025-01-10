7b806c171a28492f948be0a32b45a1541052828a
G.Reijn <G.Reijn@enzazaden.nl>
2025-01-10 16:19:10 +0100

    Remove test file for offline maps

--- test.ps1
+++
@@ -1,3 +0,0 @@
-﻿$class = [Windows.Services.Maps.OfflineMaps.OfflineMapPackage, Windows.Services.Maps.OfflineMaps, ContentType = WindowsRuntime]
-
-# https://github.com/PowerShell/PowerShell/issues/13042#issuecomment-653357546
