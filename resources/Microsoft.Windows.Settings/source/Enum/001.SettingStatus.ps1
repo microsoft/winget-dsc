23b6a9ad51b609e8ceeb84e21e9bc15c189bfac4
G.Reijn <G.Reijn@enzazaden.nl>
2025-02-16 13:08:55 +0100

    Forbidden pattern

--- resources/Microsoft.Windows.Settings/source/Enum/001.SettingStatus.ps1
+++ resources/Microsoft.Windows.Settings/source/Enum/001.SettingStatus.ps1
@@ -3,7 +3,7 @@
         The possible states for the DSC resource parameter SettingStatus.
         This enum is used to specify the desired state of settings that can be enabled or disabled e.g. have a value of 0 or 1.
 
-        To opt in for enums through the DscResource.Base class, set the property FeatureOptionalEnums to $true in the constructor of the DSC resource class.
+        To opt-in for enums through the DscResource.Base class, set the property FeatureOptionalEnums to $true in the constructor of the DSC resource class.
 #>
 enum SettingStatus
 {
